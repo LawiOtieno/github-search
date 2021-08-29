5b2c7370e92f85d00e5e19ae3b6bd92ef991aa32
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-08-30 01:59:40 +0300

    user profile pactured

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -9,28 +9,28 @@ import { User } from 'src/app/user';
 })
 export class UserComponent implements OnInit {
 
-  // user!:User;
-  // repos:any;
+  user!:User;
+  repos:any;
   
-  // constructor(public userService:SearchService) {
-  //  }
-  //  searchUser(username:string){
-  //    this.userService.getProfile(username).then((success)=>{
-  //     this.user = this.userService.user;
-  //    },
-  //    (error)=>{
-  //      console.log(error)
-  //    });
-  //    this.userService.getRepo(username).then((success)=>{
-  //     this.repos = this.userService.repos;
-  //    },
-  //    (error)=>{
-  //      console.log(error)
-  //    });
-  //  }
+  constructor(public userService:SearchService) {
+   }
+   searchUser(username:string){
+     this.userService.getProfile(username).then((success)=>{
+      this.user = this.userService.user;
+     },
+     (error)=>{
+       console.log(error)
+     });
+    //  this.userService.getRepo(username).then((success)=>{
+    //   this.repos = this.userService.repos;
+    //  },
+    //  (error)=>{
+    //    console.log(error)
+    //  });
+   }
 
-  // ngOnInit(): void {
-  //   this.searchUser('LawiOtieno');
-  // }
+  ngOnInit(): void {
+    this.searchUser('LawiOtieno');
+  }
 
 }
