dc2e43c33033359441be7eb0fd8ecd7703a898c2
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-08-30 02:14:56 +0300

    import form & other modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// import { FormsModule } from '@angular/forms';
-// import { HttpClientModule } from '@angular/common/http';
-// import { SearchService } from './Services/search.service';
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { UserComponent } from './components/user/user.component';
-// import { SearchComponent } from './components/search/search.component';
-// import { DateCountPipe } from './date-count.pipe';
-// import { NavbarComponent } from './components/navbar/navbar.component';
-// import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SearchService } from './Services/search.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { SearchComponent } from './components/search/search.component';
+import { DateCountPipe } from './date-count.pipe';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
