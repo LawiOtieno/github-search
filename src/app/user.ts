c7101d611ab004437ed82d79891ab5b913092180
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-08-30 02:10:56 +0300

    user profile properties declared

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,12 +1,12 @@
-// export class User {
-//     constructor(
-//         public name:string,
-//         public followers:number,
-//         public following:number,
-//         public public_repos:number,
-//         public url:string,
-//         public avatar_url:string,
-//         public login:string,
-//         // public bio:string,
-//         ){}
-// }
+export class User {
+    constructor(
+        public name:string,
+        public followers:number,
+        public following:number,
+        public public_repos:number,
+        public url:string,
+        public avatar_url:string,
+        public login:string,
+        // public bio:string,
+        ){}
+}
