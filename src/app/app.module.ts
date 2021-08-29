ec1f852b0c250e46cd49d1b601405c29d1a80515
LAWRENCE OTIENO <lawrence.otieno@student.moringaschool.com>
2021-08-29 22:13:46 +0300

    github search username

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { SearchService } from './Services/search.service';
+// import { AppRoutingModule } from './app-routing.module';
+// import { AppComponent } from './app.component';
+// import { UserComponent } from './components/user/user.component';
+// import { SearchComponent } from './components/search/search.component';
+// import { DateCountPipe } from './date-count.pipe';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     UserComponent,
+//     SearchComponent,
+//     DateCountPipe
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule,
+//     FormsModule,
+//     HttpClientModule,
+//   ],
+//   providers: [SearchService],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
